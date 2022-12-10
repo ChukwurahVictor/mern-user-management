a7ab159650865264ef76b182e510ffca63a98d12
Victor Webster <chukwurahvictor7@gmail.com>
2022-12-10 09:48:22 +0100

    Set up static folder in production

--- server.js
+++ server.js
@@ -30,6 +30,13 @@ app.use((req, res, next) => {
 // Error handling
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+
 // Port config
 const port = process.env.PORT || 5000;
 
